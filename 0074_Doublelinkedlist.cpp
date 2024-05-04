22270050e3c53c73a664f6ff3b0d057007a92d4f
HaniefuL <haniefulgeofarry@gmail.com>
2024-05-05 05:59:48 +0700

    menuliskan if

--- 0074_Doublelinkedlist.cpp
+++ 0074_Doublelinkedlist.cpp
@@ -76,3 +76,12 @@ bool search(int rollNo, Node** previous, Node** current)
 }
 
 void deleteNode()
+{
+	Node* previous, * current;
+	int rollNo;
+
+	cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+	cin >> rollNo;
+
+	if (START == NULL)
+}
